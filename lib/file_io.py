85d8f02eb539fa4e9362178b9934af2a4695b1d8
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-12-05 12:16:28 +0300

    lab done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,30 @@
-def write_file(file_name, file_content):
-    pass
+import os
+from pathlib import Path
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+
+    file_name = str(file_name) + ".txt" if not str(file_name).endswith(".txt") else str(file_name)
+
+
+    with open(file_name, "w") as file:
+        file.write(file_content)
+
+def append_file(file_name, file_content):
+    file_name = str(file_name) + ".txt" if not str(file_name).endswith(".txt") else str(file_name)
+    with open(file_name, "a") as file:
+        file.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name) + ".txt" if not str(file_name).endswith(".txt") else str(file_name)
+
+    
+    try:
+        with open(file_name, "r") as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return f"File '{file_name}' not found."
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
